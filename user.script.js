5f13b6d3ddfff3b8bca2fd9774ae48ce86f6bfb2
jean-nicolas NEYRET <38073474+bigbgjn@users.noreply.github.com>
2022-04-04 15:50:36 +0200

    Create user.script.js

---
+++ user.script.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name         Inscryption r/place
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  stoat
+// @author       oralekin (stoatified by Jeck0_0)
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://cdn.discordapp.com/attachments/413985144219893760/959922796966854706/Stoat.png
+// @grant        none
+// ==/UserScript==
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+            document.getElementsByTagName("ece")[0].shadowRoot.children[0].getElementsByTagName("ece")[0].shadowRoot.children[0].appendChild(
+        (function () {
+            if (!Date.now) {
+                Date.now = function() { return new Date().getTime(); }
+            }
+            const i = document.createElement("img");
+            i.src = "https://cdn.discordapp.com/attachments/413985144219893760/960509161995636777/frenchOverlay.png";
+            i.style = "position: absolute;left: 1471;top: 300px;image-rendering: pixelated;width: 2000px;height: 2000px;";
+            i.id = "mcss-overlay"
+            i.setAttribute("vis",1)
+            console.log(i);
+            return i;
+        })())
+
+            document.getElementsByTagName("body")[0].appendChild(
+        (function () {
+            const i = document.createElement("button");
+            i.style = "position: fixed;left: 1471px; background: #fff; top: 50%;width: 2000px;height: 2000px;";
+            i.innerHTML = "ECE"
+            i.onclick = function() {
+                const img = document.getElementsByTagName("ece")[0].shadowRoot.children[0].getElementsByTagName("ece")[0].shadowRoot.querySelector("#mcss-overlay")
+                if (img.getAttribute("vis") == "1") {
+                    img.setAttribute("vis",0)
+                    img.style.opacity  = 0
+                } else {
+
+                    img.setAttribute("vis",1)
+                    img.style.opacity  = 1
+                }
+            }
+            console.log(i);
+            return i;
+        })())
+
+    }, false);
+
+}
